41b6da8528dd951deb136dd9427de2ea3df2f29b
AugustoGustavo <gustavo_augustosj@hotmail.com>
2023-02-10 00:19:47 -0300

    new fields added

--- opentickets.py
+++ opentickets.py
@@ -10,11 +10,13 @@ content = zenpy_client.views.execute(view=tokens.__view_opentickets__)
 # Perform a simple for
 for i in range(content.count):
     row = content.next()
-    print("Ticket: ", row.ticket_id)
-    print("Assunto: ", row.subject)
-    print("Solicitante: ", row.requester)
-    print("Prioridade: ", row.priority)
-    print("Data de criação: ", row.created)
-    print("Grupo: ", row.group)
+    print("Ticket: ", row.ticket_id) ##int
+    print("Assunto: ", row.subject) ##varchar 
+    print("Solicitante: ", row.requester.name) ##varchar
+    print("Prioridade: ", row.priority) ##varchar
+    print("Data de criação: ", row.created) ##varchar
+    print("Grupo: ", row.group) ##varchar
+    print("Analista: ", row.custom_fields[0]["name"]) ##varchar
+    print("ID Organização: ", row.organization_id) ##int
     print("\n")
     
